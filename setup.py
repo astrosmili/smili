7765bd73f45b9fbd308104f69c9a6063612f5f90
Kazunori Akiyama <kazu@haystack.mit.edu>
2019-06-12 17:55:17 -0400

    modify setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.md"), encoding='utf-8') as f:
     long_description = f.read()
 
-#if path.isfile(path.join(here, "smili/libmfista.so")):
-#    errmsg="Apparently, you have not compiled C/Fortran libraries with make."
-#    errmsg+=" Please install this library by 'make install' not by 'python setup.py install'"
-#    raise RuntimeError(errmsg)
+if path.isfile(path.join(here, "smili/libmfista.so")):
+    errmsg="Apparently, you have not compiled C/Fortran libraries with make."
+    errmsg+=" Please install this library by 'make install' not by 'python setup.py install'"
+    raise RuntimeError(errmsg)
 
 setup(
     name="smili",
@@ -25,5 +25,5 @@ setup(
     package_data={'smili': ['*.so','imaging/*.so']},
     install_requires = [
         "future","numpy","scipy","matplotlib","pandas",
-        "scikit-image","astropy","tqdm","theano","pyds9"]
+        "scikit-image","astropy<=2.0.14","tqdm","theano","pyds9"]
 )
