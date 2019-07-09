42fc53ae6d112ea0fb93eeeeed03090d58c9e515
Kazunori Akiyama <kazu@haystack.mit.edu>
2019-07-09 11:48:43 -0400

    remove theano dependency from geomodel modules, adding triangular/rectangular functions

--- smili/geomodel/__init__.py
+++ smili/geomodel/__init__.py
@@ -6,4 +6,4 @@ This is a sub-module of smili handling image fits data.
 __author__ = "Smili Developer Team"
 
 from . import geomodel
-from .geomodel import GeoModel, Gaussian
+from .geomodel import GeoModel, Gaussian, Rectangular, Triangular
--- smili/geomodel/geomodel_theano.py
+++ smili/geomodel/geomodel_theano.py
@@ -9,115 +9,135 @@ Lindy Blackburn's python module 'modmod', and
 we would like to thank Lindy to share his idea.
 '''
 import numpy as np
+import theano
+import theano.tensor as T
 from .. import util
 
+# initial variables
+x_T, y_T = T.scalars("x","y")
+u_T, v_T = T.scalars("u","v")
+u1_T, v1_T = T.scalars("u1","v1")
+u2_T, v2_T = T.scalars("u2","v2")
+u3_T, v3_T = T.scalars("u3","v3")
+u4_T, v4_T = T.scalars("u4","v4")
+
 class GeoModel(object):
-    def __init__(self, V=None, I=None):
-        if V is None:
-            self.V = lambda u, v: (u-u)*(v-v)
+    def __init__(self, Vreal=None, Vimag=None, I=None):
+        if Vreal is None:
+            self.Vreal = lambda u=u_T, v=v_T: (u-u)*(v-v)
         else:
-            self.V = V
+            self.Vreal = Vreal
+
+        if Vimag is None:
+            self.Vimag = lambda u=u_T, v=v_T: (u-u)*(v-v)
+        else:
+            self.Vimag = Vimag
 
         if I is None:
-            self.I = lambda x, y: (x-x)*(y-y)
+            self.I = lambda x=x_T, y=y_T: (x-x)*(y-y)
         else:
             self.I = I
 
     def __add__(self, other):
         if type(self) == type(other):
-            V = lambda u, v: self.V(u,v) + other.V(u,v)
-            I = lambda x, y: self.I(x,y) + other.I(x,y)
-            return GeoModel(V=V, I=I)
+            Vreal = lambda u=u_T, v=v_T: self.Vreal(u,v) + other.Vreal(u,v)
+            Vimag = lambda u=u_T, v=v_T: self.Vimag(u,v) + other.Vimag(u,v)
+            I = lambda x=x_T, y=y_T: self.I(x,y) + other.I(x,y)
+            return GeoModel(Vreal=Vreal, Vimag=Vimag, I=I)
         else:
             raise ValueError("Addition can be calculated only between the same type of objects")
 
     def __iadd__(self, other):
         if type(self) == type(other):
-            V = lambda u, v: self.V(u,v) + other.V(u,v)
-            I = lambda x, y: self.I(x,y) + other.I(x,y)
-            return GeoModel(V=V, I=I)
+            Vreal = lambda u=u_T, v=v_T: self.Vreal(u,v) + other.Vreal(u,v)
+            Vimag = lambda u=u_T, v=v_T: self.Vimag(u,v) + other.Vimag(u,v)
+            I = lambda x=x_T, y=y_T: self.I(x,y) + other.I(x,y)
+            return GeoModel(Vreal=Vreal, Vimag=Vimag, I=I)
         else:
             raise ValueError("Addition can be calculated only between the same type of objects")
 
     def __sub__(self, other):
         if type(self) == type(other):
-            V = lambda u, v: self.V(u,v) - other.V(u,v)
-            I = lambda x, y: self.I(x,y) - other.I(x,y)
-            return GeoModel(V=V, I=I)
+            Vreal = lambda u=u_T, v=v_T: self.Vreal(u,v) - other.Vreal(u,v)
+            Vimag = lambda u=u_T, v=v_T: self.Vimag(u,v) - other.Vimag(u,v)
+            I = lambda x=x_T, y=y_T: self.I(x,y) - other.I(x,y)
+            return GeoModel(Vreal=Vreal, Vimag=Vimag, I=I)
         else:
             raise ValueError("Subtraction can be calculated only between the same type of objects")
 
 
     def __isub__(self, other):
         if type(self) == type(other):
-            V = lambda u, v: self.V(u,v) - other.V(u,v)
-            I = lambda x, y: self.I(x,y) - other.I(x,y)
-            return GeoModel(V=V, I=I)
+            Vreal = lambda u=u_T, v=v_T: self.Vreal(u,v) - other.Vreal(u,v)
+            Vimag = lambda u=u_T, v=v_T: self.Vimag(u,v) - other.Vimag(u,v)
+            I = lambda x=x_T, y=y_T: self.I(x,y) - other.I(x,y)
+            return GeoModel(Vreal=Vreal, Vimag=Vimag, I=I)
         else:
             raise ValueError("Subtraction can be calculated only between the same type of objects")
 
 
     def __mul__(self, other):
-        V = lambda u, v: self.V(u,v) * other
-        I = lambda x, y: self.I(x,y) * other
-        return GeoModel(V=V, I=I)
+        Vreal = lambda u=u_T, v=v_T: self.Vreal(u,v) * other
+        Vimag = lambda u=u_T, v=v_T: self.Vimag(u,v) * other
+        I = lambda x=x_T, y=y_T: self.I(x,y) * other
+        return GeoModel(Vreal=Vreal, Vimag=Vimag, I=I)
 
 
     def __imul__(self, other):
-        V = lambda u, v: self.V(u,v) * other
-        I = lambda x, y: self.I(x,y) * other
-        return GeoModel(V=V, I=I)
+        Vreal = lambda u=u_T, v=v_T: self.Vreal(u,v) * other
+        Vimag = lambda u=u_T, v=v_T: self.Vimag(u,v) * other
+        I = lambda x=x_T, y=y_T: self.I(x,y) * other
+        return GeoModel(Vreal=Vreal, Vimag=Vimag, I=I)
 
 
     def __truediv__(self, other):
-        V = lambda u, v: self.V(u,v) / other
-        I = lambda x, y: self.I(x,y) / other
-        return GeoModel(V=V, I=I)
+        Vreal = lambda u=u_T, v=v_T: self.Vreal(u,v) / other
+        Vimag = lambda u=u_T, v=v_T: self.Vimag(u,v) / other
+        I = lambda x=x_T, y=y_T: self.I(x,y) / other
+        return GeoModel(Vreal=Vreal, Vimag=Vimag, I=I)
 
 
     def __itruediv__(self, other):
-        V = lambda u, v: self.V(u,v) / other
-        I = lambda x, y: self.I(x,y) / other
-        return GeoModel(V=V, I=I)
+        Vreal = lambda u=u_T, v=v_T: self.Vreal(u,v) / other
+        Vimag = lambda u=u_T, v=v_T: self.Vimag(u,v) / other
+        I = lambda x=x_T, y=y_T: self.I(x,y) / other
+        return GeoModel(Vreal=Vreal, Vimag=Vimag, I=I)
 
 
     def shift(self, deltax=0., deltay=0., angunit="mas"):
         angunit2rad = angconv(angunit, "rad")
         dx = deltax * angunit2rad
         dy = deltay * angunit2rad
-        dphase = 2*np.pi*(u*dx+v*dy)
-        V = lambda u, v: self.V(u,v) * np.exp(1j*dphase)
-        I = lambda x, y: self.I(x-dx,y-dy)
-        return GeoModel(V=V, I=I)
+        Vreal = lambda u=u_T, v=v_T: self.Vreal(u,v) * T.cos(2*np.pi*(u*dx+v*dy)) - self.Vimag(u,v) * T.sin(2*np.pi*(u*dx+v*dy))
+        Vimag = lambda u=u_T, v=v_T: self.Vreal(u,v) * T.sin(2*np.pi*(u*dx+v*dy)) + self.Vimag(u,v) * T.cos(2*np.pi*(u*dx+v*dy))
+        I = lambda x=x_T, y=y_T: self.I(x-dx,y-dy)
+        return GeoModel(Vreal=Vreal, Vimag=Vimag, I=I)
 
     def rotate(self, deltaPA=0., deg=True):
         if deg:
             dPA = deltaPA * np.pi / 180
         else:
             dPA = deltaPA
-        cosdpa = np.cos(dPA)
-        sindpa = np.sin(dPA)
-        x1 = lambda x, y: x * cosdpa - y * sindpa
-        y1 = lambda x, y: x * sindpa + y * cosdpa
-        V = lambda u, v: self.V(x1(u,v),y1(u,v))
-        I = lambda x, y: self.I(x1(x,y),y1(x,y))
-        return GeoModel(V=V, I=I)
+        cosdpa = T.cos(dPA)
+        sindpa = T.sin(dPA)
+        x1 = lambda x=x_T, y=y_T: x * cosdpa - y * sindpa
+        y1 = lambda x=x_T, y=y_T: x * sindpa + y * cosdpa
+        Vreal = lambda u=u_T, v=v_T: self.Vreal(x1(u,v),y1(u,v))
+        Vimag = lambda u=u_T, v=v_T: self.Vimag(x1(u,v),y1(u,v))
+        I = lambda x=x_T, y=y_T: self.I(x1(x,y),y1(x,y))
+        return GeoModel(Vreal=Vreal, Vimag=Vimag, I=I)
 
 
     def scale(self, hx=1., hy=None):
         if hy is None:
             hy = hx
-        V = lambda u, v: self.V(u*hx, v*hy)
-        I = lambda x, y: self.I(x/hx,y/hy)/hx/hy
-        return GeoModel(V=V, I=I)
+        Vreal = lambda u=u_T, v=v_T: self.Vreal(u*hx, v*hy)
+        Vimag = lambda u=u_T, v=v_T: self.Vimag(u*hx, v*hy)
+        I = lambda x=x_T, y=y_T: self.I(x/hx,y/hy)/hx/hy
+        return GeoModel(Vreal=Vreal, Vimag=Vimag, I=I)
 
-    def Vre(self, u, v):
-        return np.real(self.V(u,v))
 
-    def Vim(self, u, v):
-        return np.imag(self.V(u,v))
-
-    def Vamp(self, u, v):
+    def Vamp(self, u=u_T, v=v_T):
         '''
         Return theano symbolic represenation of the visibility amplitude
 
@@ -126,10 +146,12 @@ class GeoModel(object):
         Return:
             amplitude
         '''
-        return np.abs(self.V(u,v))
+        Vre = self.Vreal(u,v)
+        Vim = self.Vimag(u,v)
+        return T.sqrt(Vre*Vre + Vim*Vim)
 
 
-    def logVamp(self, u, v):
+    def logVamp(self, u=u_T, v=v_T):
         '''
         Return theano symbolic represenation of the visibility amplitude
 
@@ -138,10 +160,10 @@ class GeoModel(object):
         Return:
             amplitude
         '''
-        return np.log(self.Vamp(u,v))
+        return T.log(self.Vamp(u,v))
 
 
-    def Vphase(self, u, v):
+    def Vphase(self, u=u_T, v=v_T):
         '''
         Return theano symbolic represenation of the visibility phase
 
@@ -150,11 +172,11 @@ class GeoModel(object):
         Return:
             phase in radian
         '''
-        return np.angle(self.V(u,v))
+        return T.arctan2(self.Vimag(u,v), self.Vreal(u,v))
 
 
     # Bi-spectrum
-    def B(self, u1, v1, u2, v2, u3, v3):
+    def Bre(self, u1=u1_T, v1=v1_T, u2=u2_T, v2=v2_T, u3=u3_T, v3=v3_T):
         '''
         Return theano symbolic represenation of the real part of the Bi-spectrum.
 
@@ -163,12 +185,17 @@ class GeoModel(object):
         Return:
             real part of the bi-spectrum
         '''
-        V1 = self.V(u1,v1)
-        V2 = self.V(u2,v2)
-        V3 = self.V(u3,v3)
-        return V1*V2*V3
+        Vre1 = self.Vreal(u1,v1)
+        Vim1 = self.Vimag(u1,v1)
+        Vre2 = self.Vreal(u2,v2)
+        Vim2 = self.Vimag(u2,v2)
+        Vre3 = self.Vreal(u3,v3)
+        Vim3 = self.Vimag(u3,v3)
+        Bre =  -Vim1*Vim2*Vre3 - Vim1*Vim3*Vre2 - Vim2*Vim3*Vre1 + Vre1*Vre2*Vre3
+        return Bre
 
-    def Bre(self, u1, v1, u2, v2, u3, v3):
+
+    def Bim(self, u1=u1_T, v1=v1_T, u2=u2_T, v2=v2_T, u3=u3_T, v3=v3_T):
         '''
         Return theano symbolic represenation of the imaginary part of the Bi-spectrum.
 
@@ -177,21 +204,17 @@ class GeoModel(object):
         Return:
             imaginary part of the bi-spectrum
         '''
-        return np.real(B(u1,v1,u2,v2,u3,v3))
-
-    def Bim(self, u1, v1, u2, v2, u3, v3):
-        '''
-        Return theano symbolic represenation of the imaginary part of the Bi-spectrum.
-
-        Args:
-            un, vn (n=1, 2, 3): uv-coordinates
-        Return:
-            imaginary part of the bi-spectrum
-        '''
-        return np.imag(B(u1,v1,u2,v2,u3,v3))
+        Vre1 = self.Vreal(u1,v1)
+        Vim1 = self.Vimag(u1,v1)
+        Vre2 = self.Vreal(u2,v2)
+        Vim2 = self.Vimag(u2,v2)
+        Vre3 = self.Vreal(u3,v3)
+        Vim3 = self.Vimag(u3,v3)
+        Bim = -Vim1*Vim2*Vim3 + Vim1*Vre2*Vre3 + Vim2*Vre1*Vre3 + Vim3*Vre1*Vre2
+        return Bim
 
 
-    def Bamp(self, u1, v1, u2, v2, u3, v3):
+    def Bamp(self, u1=u1_T, v1=v1_T, u2=u2_T, v2=v2_T, u3=u3_T, v3=v3_T):
         '''
         Return theano symbolic represenation of the amplitude of the Bi-spectrum.
 
@@ -200,10 +223,13 @@ class GeoModel(object):
         Return:
             amplitude of the bi-spectrum
         '''
-        return np.abs(B(u1, v1, u2, v2, u3, v3))
+        Bre = self.Bre(u1, v1, u2, v2, u3, v3)
+        Bim = self.Bim(u1, v1, u2, v2, u3, v3)
+        Bamp = T.sqrt(Bre*Bre+Bim*Bim)
+        return Bamp
 
 
-    def Bphase(self, u1, v1, u2, v2, u3, v3):
+    def Bphase(self, u1=u1_T, v1=v1_T, u2=u2_T, v2=v2_T, u3=u3_T, v3=v3_T):
         '''
         Return theano symbolic represenation of the phase of the Bi-spectrum.
         if given uv-coodinates are closed, this will be the closure phase.
@@ -213,11 +239,14 @@ class GeoModel(object):
         Return:
             phase of the bi-spectrum
         '''
-        return np.angle(B(u1, v1, u2, v2, u3, v3))
+        Bre = self.Bre(u1, v1, u2, v2, u3, v3)
+        Bim = self.Bim(u1, v1, u2, v2, u3, v3)
+        Bphase = T.arctan2(Bim, Bre)
+        return Bphase
 
 
     # Closure Amplitudes
-    def Camp(self, u1, v1, u2, v2, u3, v3, u4, v4):
+    def Camp(self, u1=u1_T, v1=v1_T, u2=u2_T, v2=v2_T, u3=u3_T, v3=v3_T, u4=u4_T, v4=v4_T):
         '''
         Return theano symbolic represenation of the phase of the Bi-spectrum.
         if given uv-coodinates are closed, this will be the closure phase.
@@ -234,7 +263,7 @@ class GeoModel(object):
         return Vamp1*Vamp2/Vamp3/Vamp4
 
 
-    def logCamp(self, u1, v1, u2, v2, u3, v3, u4, v4):
+    def logCamp(self, u1=u1_T, v1=v1_T, u2=u2_T, v2=v2_T, u3=u3_T, v3=v3_T, u4=u4_T, v4=v4_T):
         '''
         Return theano symbolic represenation of the phase of the Bi-spectrum.
         if given uv-coodinates are closed, this will be the closure phase.
@@ -256,7 +285,7 @@ class GeoModel(object):
 #-------------------------------------------------------------------------------
 def dphase(phase1, phase2):
     dphase = phase2 - phase1
-    return np.arctan2(np.sin(dphase), np.cos(dphase))
+    return T.arctan2(T.sin(dphase), T.cos(dphase))
 
 def angconv(unit1="deg", unit2="deg"):
     '''
@@ -309,10 +338,10 @@ def angconv(unit1="deg", unit2="deg"):
 def phaseshift(u,v,x0=0,y0=0,angunit="mas"):
     '''
     Phase of a symmetric object (Gaussians, Point Sources, etc).
-    This function also can be used to compute a phase shift due to positional shifnp.
+    This function also can be used to compute a phase shift due to positional shift.
     Args:
         u, v (mandatory): uv coordinates in lambda
-        x0=0, y0=0: position of centorid or positional shift in anguninp.
+        x0=0, y0=0: position of centorid or positional shift in angunit.
         angunit="mas": angular unit of x0, y0 (uas, mas, asec, amin, deg, rad)
     return:
         phase in rad
@@ -341,95 +370,13 @@ def Gaussian(x0=0,y0=0,totalflux=1,majsize=1,minsize=None,pa=0,angunit="mas"):
 
     # define a Gaussian with F=1 jy, size = 1 (angunit)
     sigma = angconv(angunit, "rad")/np.sqrt(8*np.log(2))
-    V = lambda u, v: np.exp(-2*np.pi*np.pi*(u*u+v*v)*sigma*sigma)
-    I = lambda x, y: 1/2/np.pi/sigma/sigma*np.exp(-(x*x+y*y)/2/sigma/sigma)
-    output = GeoModel(V=V, I=I)
+    Vreal = lambda u=u_T, v=v_T: T.exp(-2*np.pi*np.pi*(u*u+v*v)*sigma*sigma)
+    I = lambda x=x_T, y=y_T: 1/2/np.pi/sigma/sigma*T.exp(-(x*x+y*y)/2/sigma/sigma)
+    output = GeoModel(Vreal=Vreal, I=I)
 
     # transform Gaussian, so that it will be elliptical Gaussian
-    if totalflux != 1:
-        output = output * totalflux
-    if majsize != 1 or minsize != 1:
-        output = output.scale(hx=minsize, hy=majsize)
-    if pa != 0:
-        output = output.rotate(deltaPA=pa, deg=True)
-    if x0 != 0 or y0 != 0:
-        output = output.shift(deltax=x0, deltay=y0, angunit=angunit)
-    return output
-
-
-def Rectangular(totalflux=1,x0=0,y0=0,dx=1,dy=None,angunit="mas"):
-    '''
-    Create geomodel.geomodel.GeoModel Object for the specified RectAngular function
-    Args:
-        x0=0.0, y0=0.0: position of the centorid
-        totalflux=1.0: total flux of the Gaussian
-        dx, dy: the size of the rectangular function
-        angunit="mas": angular unit of x0, y0, dx, dy (uas, mas, asec, amin, deg, rad)
-    Returns:
-        geomodel.geomodel.GeoModel Object for the specified RectAngular function
-    '''
-    if dy is None:
-        dy = dx
-
-    # define a Gaussian with F=1 jy, size = 1 (angunit)
-    conv = angconv(angunit, "rad")
-    dxrad = np.abs(conv*dx)
-    dyrad = np.abs(conv*dy)
-    dxyinv = 1./dxrad/dyrad
-
-    def I(x,y):
-        xnorm = np.abs(x/dxrad)
-        ynorm = np.abs(y/dyrad)
-        return np.where(xnorm<=0.5, 1, 0)*np.where(ynorm<=0.5, 1, 0)
-
-    def V(u,v):
-        unorm = np.pi*dxrad*u
-        vnorm = np.pi*dyrad*v
-        return np.sin(unorm)*np.sin(vnorm)/unorm/vnorm
-
-    output = GeoModel(V=V, I=I)
-    if totalflux != 1:
-        output = output * totalflux
-    if x0 != 0 or y0 != 0:
-        output = output.shift(deltax=x0, deltay=y0, angunit=angunit)
-    return output
-
-
-def Triangular(totalflux=1,x0=0,y0=0,dx=1,dy=None,angunit="mas"):
-    '''
-    Create geomodel.geomodel.GeoModel Object for the specified Triangular function
-    Args:
-        x0=0.0, y0=0.0: position of the centroid
-        totalflux=1.0: total flux of the Gaussian
-        dx, dy: the size of the Triangular function
-        angunit="mas": angular unit of x0, y0, dx, dy (uas, mas, asec, amin, deg, rad)
-    Returns:
-        geomodel.geomodel.GeoModel Object for the specified Triangular function
-    '''
-    if dy is None:
-        dy = dx
-
-    # define a Gaussian with F=1 jy, size = 1 (angunit)
-    conv = angconv(angunit, "rad")
-    dxrad = np.abs(conv*dx)
-    dyrad = np.abs(conv*dy)
-    dxyinv = 1./dxrad/dyrad
-
-    V = lambda u, v: np.exp(-2*np.pi*np.pi*(u*u+v*v)*sigma*sigma)
-
-    def I(x,y):
-        xnorm = np.abs(x/dxrad)
-        ynorm = np.abs(y/dyrad)
-        return np.where(xnorm<=1, (1-xnorm)/dxrad, 0)*np.where(ynorm<=1, (1-ynorm)/dyrad, 0)
-
-    def V(u,v):
-        unorm = np.pi*dxrad*u
-        vnorm = np.pi*dyrad*v
-        return np.square(np.sin(unorm)*np.sin(vnorm)/unorm/vnorm)
-
-    output = GeoModel(V=V, I=I)
-    if totalflux != 1:
-        output = output * totalflux
-    if x0 != 0 or y0 != 0:
-        output = output.shift(deltax=x0, deltay=y0, angunit=angunit)
+    output = output * totalflux
+    output = output.scale(hx=minsize, hy=majsize)
+    output = output.rotate(deltaPA=pa, deg=True)
+    output = output.shift(deltax=x0, deltay=y0, angunit=angunit)
     return output
