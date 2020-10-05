6754dc822426cbd7eee7f7d6b82d3c8ec7b25a0d
Kazu Akiyama <kazunori.akiyama.kazu@gmail.com>
2020-10-05 12:07:13 -0400

    add zip_safe=False to setup.py, since it will cause a bug in python 3.8

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from codecs     import open
 from os         import path
 
 here = path.abspath(path.dirname(__file__))
+
 with open(path.join(here, "README.md"), encoding='utf-8') as f:
     long_description = f.read()
 
-if path.isfile(path.join(here, "smili/libmfista.so")):
-    errmsg="Apparently, you have not compiled C/Fortran libraries with make."
-    errmsg+=" Please install this library by 'make install' not by 'python setup.py install'"
-    raise RuntimeError(errmsg)
+#if path.isfile(path.join(here, "smili/libmfista.so")):
+#    errmsg="Apparently, you have not compiled C/Fortran libraries with make."
+#    errmsg+=" Please install this library by 'make install' not by 'python setup.py install'"
+#    raise RuntimeError(errmsg)
 
 setup(
     name="smili",
@@ -20,9 +21,10 @@ setup(
     author = "Kazunori Akiyama",
     author_email = "kakiyama@mit.edu",
     license = "GPL ver 3.0",
+    zip_safe=False,
     keywords = "imaging astronomy EHT",
     packages = find_packages(exclude=["doc*", "test*"]),
-    package_data={'smili': ['*.so','imaging/*.so']},
+    package_data={'smili': ['*.so*','imaging/*.so*']},
     install_requires = [
         "numpy","scipy","matplotlib","pandas",
         "scikit-image","astropy","tqdm","theano",
